d2b024d9abfe5dc0eedc10c2f5e98d24bd275ed4
Segfaulter <brudnevskij@users.noreply.github.com>
2026-05-21 16:04:39 +0300

    refactor: exported the aggregator structs

--- src/aggregator.rs
+++ src/aggregator.rs
@@ -2,24 +2,24 @@ use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
 #[derive(Debug, Clone, Deserialize)]
-struct Message {
+pub struct Message {
     key: String,
     value: i64,
 }
 
 #[derive(Debug, Clone, Serialize)]
-struct AggregatedMessage {
+pub struct AggregatedMessage {
     key: String,
     value: i64,
 }
 
 #[derive(Debug, Default)]
-struct Aggregator {
+pub struct Aggregator {
     state: HashMap<String, i64>,
 }
 
 impl Aggregator {
-    fn process(&mut self, msg: Message) -> AggregatedMessage {
+    pub fn process(&mut self, msg: Message) -> AggregatedMessage {
         let sum = self.state.entry(msg.key.clone()).or_insert(0);
         *sum += msg.value;
 
