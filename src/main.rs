234e27b155d8df39a7d2628beb0dea98b2a3aa3c
Segfaulter <brudnevskij@users.noreply.github.com>
2026-05-21 15:51:32 +0300

    feat: added connection to sample redis, without error handling

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod aggregator;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    #[arg(short, long)]
+    #[arg(short, long, default_value = "redis://127.0.0.1/")]
     redis_url: String,
 
     #[arg(long, default_value = "inputA,inputB,inputC")]
@@ -18,5 +18,10 @@ struct Args {
 #[tokio::main]
 async fn main() {
     let args = Args::parse();
-    println!("{:?}", args);
+
+    let client = redis::Client::open(args.redis_url.clone()).unwrap();
+    let mut conn = client.get_multiplexed_async_connection().await.unwrap();
+
+    let pong: String = redis::cmd("PING").query_async(&mut conn).await.unwrap();
+    println!("{pong}");
 }
