01bd5f2a6ddce33222f44e822c1a7e3239dc28b1
Segfaulter <brudnevskij@users.noreply.github.com>
2026-05-21 15:16:42 +0300

    feat: added dependencies, stub main cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    redis_url: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
+    println!("{:?}", args);
 }
